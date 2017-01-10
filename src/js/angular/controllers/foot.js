b06ad32331f76de2bf2e93f0739d7e1078a2b0bd
marunpeng93 <1643646033@qq.com>
2017-01-10 13:33:26 +0800

    foot.js改变

--- src/js/angular/controllers/foot.js
+++ src/js/angular/controllers/foot.js
@@ -1,6 +1,7 @@
 angular.module("axf")
 .controller("footCtrl",["$scope",function($scope){
 	$scope.num=0;
+	location.hash="#/home"
 	//图片数组
 	$scope.thatimg = ["images/footer-1.png","images/footer-2.png","images/footer-3.png","images/footer-4.png","images/footer-5.png"];
 	$scope.thisimg = ["images/footer-12.png","images/footer-22.png","images/footer-32.png","images/footer-42.png","images/footer-52.png"]
